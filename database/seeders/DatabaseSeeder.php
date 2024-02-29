8d29c5c0315f54398b7f8377ed62b9c7ebb6de5a
khaalid <khaalidm.m@gmail.com>
2024-02-29 16:02:24 +0200

    Seeded a User for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Worker;
 use Illuminate\Database\Seeder;
 
@@ -13,12 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Test',
+             'email' => 'test@example.com',
+             'password' => bcrypt('1234password'),
+         ]);
 
         Worker::factory(5)->create();
     }
